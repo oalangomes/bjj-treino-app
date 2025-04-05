bef7bc119033dd4cf4f336c16f598c584bc3d273
Alan Gomes <52175262+oalangomes@users.noreply.github.com>
2025-04-05 05:07:52 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open('treino-jj').then(function(cache) {
+    caches.open('treino-jj').then(function (cache) {
       return cache.addAll([
+        './',
         'index.html',
         'manifest.json',
         'icon-192.png',
@@ -11,10 +11,13 @@ self.addEventListener('install', function(e) {
     })
   );
 });
-self.addEventListener('fetch', function(e) {
+
+self.addEventListener('fetch', function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+    fetch(e.request).catch(function () {
+      return caches.match(e.request).then(function (response) {
+        return response || caches.match('index.html');
+      });
     })
   );
 });
